5768d03954c996f5f511328dd9fff69b0cb9fd1f
Md Rezwanur Rahman <130373403+Mahadi2478@users.noreply.github.com>
2023-12-11 21:11:02 +0600

    Add files via upload

---
+++ BST_traverse.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <queue>  // Include the queue header
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int d) {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* insertintoBST(Node* root, int d) {
+    if (root == NULL) {
+        root = new Node(d);
+        return root;
+    }
+
+    if (d > root->data) {
+        root->right = insertintoBST(root->right, d);  // insert at right part
+    } else {
+        root->left = insertintoBST(root->left, d);
+    }
+    return root;
+}
+
+void takeinput(Node*& root) {
+    int data;
+    cin >> data;
+    while (data != -1) {
+        root = insertintoBST(root, data);
+        cin >> data;
+    }
+}
+
+void levelordertraversal(Node* root) {
+    if (root == NULL) {
+        return;
+    }
+
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty()) {
+        Node* temp = q.front();
+        q.pop();
+
+        if (temp == NULL) {
+            cout << endl;  // previous level has been completely traversed
+            if (!q.empty()) {
+                q.push(NULL);  // queue still has some child nodes
+            }
+        } else {
+            cout << temp->data << " ";
+            if (temp->left) {
+                q.push(temp->left);
+            }
+            if (temp->right) {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+int main() {
+    Node* root = NULL;
+    cout << "Enter data to create BST" << endl;
+    takeinput(root);
+    cout << "Printing the BST" << endl;
+    levelordertraversal(root);
+
+    return 0;  // Add a return statement in the main function
+}
